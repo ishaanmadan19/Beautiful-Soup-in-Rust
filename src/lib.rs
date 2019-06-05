12219e24b30cb54beedd18aa360d30559d1c9406
David Nelson <davidnelson2019@u.northwestern.edu>
2019-06-05 16:17:35 -0500

    iterators

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,139 @@
 
+
 pub mod bsr {
+    use std::collections::HashMap;
+    extern crate reqwest;
+
+    struct ParseTree<'a> {
+        roots: Vec<HTMLContent>,
+        stack: Option<Vec<&'a HTMLContent>>,
+    }
+
+    struct Tag {
+        tag_type: String,
+//        attributes: Vec<String>, // not sure which to use, hashmap bigger but clunkier
+        attributes: HashMap<String, String>,
+        content: Vec<HTMLContent>,
+    }
+
+    enum HTMLContent {
+        Raw(String),
+        Tag(Box<Tag>),
+    }
+
+
+    // Would be more clever and better to allow these functions to be applied on top of each other
+    // I don't believe that works currently...
+
+    // Also, should implement BFS iterator variant...
+
+    impl ParseTree {
+
+        pub fn new(url: &str) -> Self {
+            parse_html(get_html(url))
+        }
+
+        fn get_html(url: &str) -> String {
+            // do error handling instead of unwrap later
+            reqwest::get(url).unwrap().text().unwrap()
+        }
+
+        fn parse_html(html: String) -> Self {
+            ParseTree {
+                roots: Vec::new(),
+                stack: None,
+            }
+        }
+
+        // should this return HTMLContent's or Tag's
+        pub fn find_tags(&self, html_tag_type: String) -> impl Iterator<Item = HTMLContent> {
+
+            // which soln is better, returning htmlcontents or tags
+
+            // returns HTMLContent's
+            self.iter().filter(move |node| match node { // do i need to deref the tag/maybe patter match of of box?
+                                                HTMLContent::Raw(_) => false,
+                                                HTMLContent::Tag(box_tag) => box_tag.tag_type == html_tag_type
+                                            })
+                // returns tags
+//            self.iter().filter_map(|node|   if let HTMLContent::Tag(Box(html_tag)) = node { // i box pattern match needed/good?
+//                if html_tag.tag_type == html_tag_type {
+//                    Some(tag)
+//                }
+//                else {
+//                    None
+//                }
+//            } else {
+//                None
+//            })
+
+        }
+
+        // Does this and the above automatically return &Item???
+        pub fn find_text(&self) -> impl Iterator<Item = String> {
+            // Which of the two is correct/better?
+
+//            self.iter().map(move |node| match node {
+//                HTMLContent::Raw(s) => s,
+//                HTMLContent::Tag(box_tag) => None
+//            });
+
+            self.iter().filter_map(|node|   if let HTMLContent::Raw(s) = node {
+                                                Some(s)
+                                            } else {
+                                                None
+                                            })
+        }
+
+        // Again, return HTMLContent's or Tag's ?
+        pub fn find_attrs(&self, attr: String, value: String) -> impl Iterator<Item = HTMLContent> {
+            self.iter().filter(move |node| match node { // do i need to deref the tag/maybe patter match of of box?
+                HTMLContent::Raw(_) => false,
+                HTMLContent::Tag(box_tag) => {
+                    match box_tag.attributes.get(&attr) {
+                        Some(val) => val==value,
+                        None => false
+                    }
+                }
+            })
+        }
 
 
 
+    }
+
+    // Do we also need into iterator to produce collections?
+    impl<T> Iterator for ParseTree {
+        // this is a DFS iterator...
+        type Item = T;
+        fn next(&mut self) -> Option<T> {
+            let stack = match self.stack.as_mut() {
+                None => {
+                    let mut temp_stack = Vec::new();
+                    for i in self.roots.iter().rev() {
+                        temp_stack.push(i);
+                    }
+                    self.stack = Some(temp_stack);
+                    self.stack.unwrap().as_mut()
+                },
+                Some(s) => s
+            };
+            if let Some(node) = stack.pop() {
+                if let HTMLContent::Tag(tag_box) = node { //add children to stack if tag object
+//                    let children = tag_box.content.iter().rev();
+                    for child in (**tag_box).content.iter().rev() {
+                        stack.push(child);
+                    }
+                }
+                return Some(node)
+            }
+//            else {
+            self.stack = None; // else is not necessary? is it idiomatic?
+            None
+//            }
+
+        }
+
+    }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,17 @@
 #[macro_use]
 extern crate nom;
 
-
-// beginning of object ideas
-
-struct ParseTree {
-    roots: Vec<HTMLContent>
+//// beginning of object ideas
+//
+struct ParseTree<'a> {
+    roots: Vec<HTMLContent>,
+    stack: Option<Vec<&'a HTMLContent>>,
 }
 
 struct Tag {
     tag_type: String,
-    attributes: Vec<String>,
+    //        attributes: Vec<String>, // not sure which to use, hashmap bigger but clunkier
+    attributes: HashMap<String, String>,
     content: Vec<HTMLContent>,
 }
 
@@ -21,80 +22,81 @@ enum HTMLContent {
     Tag(Box<Tag>),
 }
 
-
-named!(parse_html<&str, ParseTree>,
-        do_parse!(
-            tag_s!("<body>") >>
-            main: mult_parse >>
-            tag_s!("</body>")
-            >>
-            (ParseTree{roots: main})
-        )
-    );
-
-named!(mult_parse<&str, Vec<HTMLContent>,
-        many0!(complete!(parse))
-    );
-
-named!(parse<&str, HTMLContent,
-        do_parse!(
-            start: html_start_tag >>
-            c: complete!(nom::alphanumeric) >>
-            end:html_end_tag
-            >>
-            (HTML_Tag{tag_type: start.1.to_owned(), content: c.to_owned()}) //check if start tag == end tag
-        )
-    );
-
-named!(tag_parse<&str, HTMLContent,
-        do_parse!(
-            start: html_start_tag >>
-            c: complete!(nom::alphanumeric) >>
-            end:html_end_tag
-            >>
-            (HTML_Tag{tag_type: start.1.to_owned(), content: c.to_owned()}) //check if start tag == end tag
-        )
-    );
-
-named!(raw_parse<&str, HTMLContent,
-        do_parse!(
-
-        )
-    );
-
-
-
-
-named!(tag_type<&str,&str>,
-    ws!(complete!(nom::alpha)) //might be alphanumeric?
-);
-
-named!(start_tag<&str,(&str,&str,&str)>,
-        tuple!(
-            tag_s!("<"),
-            html_tag_type,
-            tag_s!(">")
-        )
-);
-
-named!(end_tag<&str,(&str,&str,&str)>,
-        tuple!(
-            tag_s!("</"),
-            html_tag_type,
-            tag_s!(">")
-        )
-);
-
-
-// IMPLEMENT SELF CLOSING TYPES, hard code which ones should be self closing???
-
-// end of object ideas
-
-// MUST PASS: <foo>baz<qux>buz</qux>baz</foo>
-
-
-
-// SOME TESTING, SEMI EXAMPLES
+//
+//
+//named!(parse_html<&str, ParseTree>,
+//        do_parse!(
+//            tag_s!("<body>") >>
+//            main: mult_parse >>
+//            tag_s!("</body>")
+//            >>
+//            (ParseTree{roots: main})
+//        )
+//    );
+//
+//named!(mult_parse<&str, Vec<HTMLContent>,
+//        many0!(complete!(parse))
+//    );
+//
+//named!(parse<&str, HTMLContent,
+//        do_parse!(
+//            start: html_start_tag >>
+//            c: complete!(nom::alphanumeric) >>
+//            end:html_end_tag
+//            >>
+//            (HTML_Tag{tag_type: start.1.to_owned(), content: c.to_owned()}) //check if start tag == end tag
+//        )
+//    );
+//
+//named!(tag_parse<&str, HTMLContent,
+//        do_parse!(
+//            start: html_start_tag >>
+//            c: multparse >>
+//            end:html_end_tag
+//            >>
+//            (HTML_Tag{tag_type: start.1.to_owned(), content: c.to_owned()}) //check if start tag == end tag
+//        )
+//    );
+//
+//named!(raw_parse<&str, HTMLContent,
+//        do_parse!(
+//
+//        )
+//    );
+//
+//
+//
+//
+//named!(tag_type<&str,&str>,
+//    ws!(complete!(nom::alpha)) //might be alphanumeric?
+//);
+//
+//named!(start_tag<&str,(&str,&str,&str)>,
+//        tuple!(
+//            tag_s!("<"),
+//            html_tag_type,
+//            tag_s!(">")
+//        )
+//);
+//
+//named!(end_tag<&str,(&str,&str,&str)>,
+//        tuple!(
+//            tag_s!("</"),
+//            html_tag_type,
+//            tag_s!(">")
+//        )
+//);
+//
+//
+//// IMPLEMENT SELF CLOSING TYPES, hard code which ones should be self closing???
+//
+//// end of object ideas
+//
+//// MUST PASS: <foo>baz<qux>buz</qux>baz</foo>
+//
+//
+//
+//// SOME TESTING, SEMI EXAMPLES
 
 
 named!(html_tag_type<&str,&str>,
