ae96dd672abc9d2e2d5625644e447c71a24d98d2
David Nelson <davidnelson2019@u.northwestern.edu>
2019-06-07 13:21:20 -0500

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,139 +1,234 @@
 
 
-pub mod bsr {
+pub mod rusty_soup {
     use std::collections::HashMap;
-    extern crate reqwest;
+    use std::collections::VecDeque;
+//    extern crate reqwest;
 
-    struct ParseTree<'a> {
-        roots: Vec<HTMLContent>,
-        stack: Option<Vec<&'a HTMLContent>>,
+    pub struct ParseTree {
+        root: HTMLContent,
     }
 
-    struct Tag {
+    pub struct Tag {
         tag_type: String,
-//        attributes: Vec<String>, // not sure which to use, hashmap bigger but clunkier
         attributes: HashMap<String, String>,
         content: Vec<HTMLContent>,
     }
 
-    enum HTMLContent {
+    pub enum HTMLContent {
         Raw(String),
         Tag(Box<Tag>),
     }
 
+    pub struct PtPreOrderIter<'a> {
+        stack: Vec<&'a HTMLContent>,
+    }
 
-    // Would be more clever and better to allow these functions to be applied on top of each other
-    // I don't believe that works currently...
+    pub struct PtLevelOrderIter<'a> {
+        queue: VecDeque<&'a HTMLContent>,
+    }
 
-    // Also, should implement BFS iterator variant...
+    impl Tag {
+        pub fn get_tag(&self) -> &String {
+            &self.tag_type
+        }
+    }
 
     impl ParseTree {
 
         pub fn new(url: &str) -> Self {
-            parse_html(get_html(url))
+            ParseTree {
+                root: ParseTree::parse_html(ParseTree::get_html(url)),
+            }
         }
 
         fn get_html(url: &str) -> String {
             // do error handling instead of unwrap later
-            reqwest::get(url).unwrap().text().unwrap()
+//            reqwest::get(url).unwrap().text().unwrap()
+            "foobar".to_owned()
         }
 
-        fn parse_html(html: String) -> Self {
-            ParseTree {
-                roots: Vec::new(),
-                stack: None,
-            }
-        }
-
-        // should this return HTMLContent's or Tag's
-        pub fn find_tags(&self, html_tag_type: String) -> impl Iterator<Item = HTMLContent> {
-
-            // which soln is better, returning htmlcontents or tags
-
-            // returns HTMLContent's
-            self.iter().filter(move |node| match node { // do i need to deref the tag/maybe patter match of of box?
-                                                HTMLContent::Raw(_) => false,
-                                                HTMLContent::Tag(box_tag) => box_tag.tag_type == html_tag_type
-                                            })
-                // returns tags
-//            self.iter().filter_map(|node|   if let HTMLContent::Tag(Box(html_tag)) = node { // i box pattern match needed/good?
-//                if html_tag.tag_type == html_tag_type {
-//                    Some(tag)
-//                }
-//                else {
-//                    None
-//                }
-//            } else {
-//                None
-//            })
+        fn parse_html(html: String) -> HTMLContent {
+            HTMLContent::Raw("link".to_owned())
 
         }
 
-        // Does this and the above automatically return &Item???
-        pub fn find_text(&self) -> impl Iterator<Item = String> {
-            // Which of the two is correct/better?
-
-//            self.iter().map(move |node| match node {
-//                HTMLContent::Raw(s) => s,
-//                HTMLContent::Tag(box_tag) => None
-//            });
-
-            self.iter().filter_map(|node|   if let HTMLContent::Raw(s) = node {
-                                                Some(s)
-                                            } else {
-                                                None
-                                            })
+        pub fn pre_iter(&self) -> impl Iterator<Item = &HTMLContent> {
+            PtPreOrderIter::new(&self.root)
         }
 
-        // Again, return HTMLContent's or Tag's ?
-        pub fn find_attrs(&self, attr: String, value: String) -> impl Iterator<Item = HTMLContent> {
-            self.iter().filter(move |node| match node { // do i need to deref the tag/maybe patter match of of box?
-                HTMLContent::Raw(_) => false,
-                HTMLContent::Tag(box_tag) => {
-                    match box_tag.attributes.get(&attr) {
-                        Some(val) => val==value,
-                        None => false
-                    }
-                }
-            })
+        pub fn level_iter(&self) -> impl Iterator<Item = &HTMLContent> {
+            PtLevelOrderIter::new(&self.root)
         }
-
-
 
     }
 
-    // Do we also need into iterator to produce collections?
-    impl<T> Iterator for ParseTree {
-        // this is a DFS iterator...
-        type Item = T;
-        fn next(&mut self) -> Option<T> {
-            let stack = match self.stack.as_mut() {
-                None => {
-                    let mut temp_stack = Vec::new();
-                    for i in self.roots.iter().rev() {
-                        temp_stack.push(i);
-                    }
-                    self.stack = Some(temp_stack);
-                    self.stack.unwrap().as_mut()
-                },
-                Some(s) => s
-            };
-            if let Some(node) = stack.pop() {
+    impl HTMLContent {
+        pub fn pre_iter(&self) -> impl Iterator<Item = &HTMLContent> {
+            PtPreOrderIter::new(self)
+        }
+
+        pub fn level_iter(&self) -> impl Iterator<Item = &HTMLContent> {
+            PtLevelOrderIter::new(self)
+        }
+    }
+
+    impl<'a> PtPreOrderIter<'a> {
+        pub fn new(content: &'a HTMLContent) -> Self {
+            PtPreOrderIter {
+                stack: vec![content],
+            }
+        }
+
+        // why can't use &self
+        pub fn find_tags(self, html_tag_type: String) -> impl Iterator<Item = &'a HTMLContent> {
+            self.filter(move |node| match node { // do i need to deref the tag/maybe patter match of of box?
+                HTMLContent::Raw(_) => false,
+                HTMLContent::Tag(box_tag) => box_tag.tag_type == html_tag_type
+            })
+        }
+
+        // why can't use &self
+        pub fn find_tags2(self, html_tag_type: String) -> impl Iterator<Item = &'a Tag> {
+            self.filter_map(move|node|   if let HTMLContent::Tag(box_tag) = node { // i box pattern match needed/good?
+                if (**box_tag).tag_type == html_tag_type {
+                    Some(&(**box_tag))
+                }
+                else {
+                    None
+                }
+            } else {
+                None
+            })
+        }
+
+//        // Does this and the above automatically return &Item???
+//        pub fn find_text(&self) -> impl Iterator<Item = String> {
+//            // Which of the two is correct/better?
+//
+////            self.iter().map(move |node| match node {
+////                HTMLContent::Raw(s) => s,
+////                HTMLContent::Tag(box_tag) => None
+////            });
+//
+//            self.iter().filter_map(|node|   if let HTMLContent::Raw(s) = node {
+//                Some(s)
+//            } else {
+//                None
+//            })
+//        }
+
+//        // Again, return HTMLContent's or Tag's ?
+//        pub fn find_attrs(&self, attr: String, value: String) -> impl Iterator<Item = HTMLContent> {
+//            self.iter().filter(move |node| match node { // do i need to deref the tag/maybe patter match of of box?
+//                HTMLContent::Raw(_) => false,
+//                HTMLContent::Tag(box_tag) => {
+//                    match box_tag.attributes.get(&attr) {
+//                        Some(val) => *val==value,
+//                        None => false
+//                    }
+//                }
+//            })
+//        }
+    }
+
+    impl<'a> Iterator for PtPreOrderIter<'a> {
+        type Item = &'a HTMLContent;
+        fn next(&mut self) -> Option<&'a HTMLContent> {
+            if let Some(node) = self.stack.pop() {
                 if let HTMLContent::Tag(tag_box) = node { //add children to stack if tag object
-//                    let children = tag_box.content.iter().rev();
                     for child in (**tag_box).content.iter().rev() {
-                        stack.push(child);
+                        self.stack.push(child);
                     }
                 }
                 return Some(node)
             }
-//            else {
-            self.stack = None; // else is not necessary? is it idiomatic?
-            None
-//            }
+            else {
+                None
+            }
 
         }
+    }
 
+    impl<'a> PtLevelOrderIter<'a> {
+        pub fn new(content: &'a HTMLContent) -> Self {
+            let mut temp_queue = VecDeque::new();
+            temp_queue.push_back(content);
+            PtLevelOrderIter {
+                queue: temp_queue,
+            }
+        }
+    }
+
+    impl<'a> Iterator for PtLevelOrderIter<'a> {
+        type Item = &'a HTMLContent;
+        fn next(&mut self) -> Option<&'a HTMLContent> {
+            if let Some(node) = self.queue.pop_front() {
+                if let HTMLContent::Tag(tag_box) = node {
+                    for child in (**tag_box).content.iter() {
+                        self.queue.push_back(child);
+                    }
+                }
+                return Some(node)
+            }
+            else {
+                None
+            }
+
+        }
+    }
+
+    impl ParseTree {
+
+        pub fn testing_tree() -> Self {
+
+            let h1 = Tag {
+                tag_type: "h1".to_owned(),
+                attributes: HashMap::new(),
+                content: vec![HTMLContent::Raw("foo".to_owned())]
+            };
+
+            let a1 = Tag {
+                tag_type: "a".to_owned(),
+                attributes: HashMap::new(),
+                content: vec![HTMLContent::Raw("link".to_owned())]
+            };
+
+            let div1 = Tag {
+                tag_type: "div".to_owned(),
+                attributes: HashMap::new(),
+                content: vec![HTMLContent::Tag(Box::new(h1)), HTMLContent::Tag(Box::new(a1))]
+            };
+
+            let a2 = Tag {
+                tag_type: "a".to_owned(),
+                attributes: HashMap::new(),
+                content: vec![HTMLContent::Raw("bar".to_owned())]
+            };
+
+            let p = Tag {
+                tag_type: "p".to_owned(),
+                attributes: HashMap::new(),
+                content: vec![HTMLContent::Raw("baz".to_owned()), HTMLContent::Tag(Box::new(a2)), HTMLContent::Raw("qux".to_owned())]
+            };
+
+            let div2 = Tag {
+                tag_type: "div".to_owned(),
+                attributes: HashMap::new(),
+                content: vec![HTMLContent::Tag(Box::new(p))]
+            };
+
+            let body = Tag {
+                tag_type: "body".to_owned(),
+                attributes: HashMap::new(),
+                content: vec![HTMLContent::Tag(Box::new(div1)),HTMLContent::Tag(Box::new(div2))]
+            };
+
+            ParseTree {
+                root: HTMLContent::Tag(Box::new(body))
+            }
+
+        }
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,26 @@
 #[macro_use]
 extern crate nom;
 
+use bsr::rusty_soup::HTMLContent;
+
 //// beginning of object ideas
 //
-struct ParseTree<'a> {
-    roots: Vec<HTMLContent>,
-    stack: Option<Vec<&'a HTMLContent>>,
-}
-
-struct Tag {
-    tag_type: String,
-    //        attributes: Vec<String>, // not sure which to use, hashmap bigger but clunkier
-    attributes: HashMap<String, String>,
-    content: Vec<HTMLContent>,
-}
-
-enum HTMLContent {
-    Raw(String),
-    Tag(Box<Tag>),
-}
+//struct ParseTree<'a> {
+//    roots: Vec<HTMLContent>,
+//    stack: Option<Vec<&'a HTMLContent>>,
+//}
+//
+//struct Tag {
+//    tag_type: String,
+//    //        attributes: Vec<String>, // not sure which to use, hashmap bigger but clunkier
+//    attributes: HashMap<String, String>,
+//    content: Vec<HTMLContent>,
+//}
+//
+//enum HTMLContent {
+//    Raw(String),
+//    Tag(Box<Tag>),
+//}
 
 //
 //
@@ -98,56 +100,81 @@ enum HTMLContent {
 //
 //// SOME TESTING, SEMI EXAMPLES
 
-
-named!(html_tag_type<&str,&str>,
-    ws!(complete!(nom::alpha)) //might be alphanumeric?
-);
-
-named!(html_start_tag<&str,(&str,&str,&str)>,
-        tuple!(
-            tag_s!("<"),
-            html_tag_type,
-            tag_s!(">")
-        )
-);
-
-named!(html_end_tag<&str,(&str,&str,&str)>,
-        tuple!(
-            tag_s!("</"),
-            html_tag_type,
-            tag_s!(">")
-        )
-);
-
-
-#[derive(Debug)]
-struct HTML_Tag {
-    tag_type: String,
-    content: String,
-}
-
-named!(get_tag<&str, HTML_Tag>,
-        do_parse!(
-            start: html_start_tag >>
-            c: complete!(nom::alphanumeric) >>
-            end:html_end_tag
-            >>
-            (HTML_Tag{tag_type: start.1.to_owned(), content: c.to_owned()}) //check if start tag == end tag
-        )
-    );
-
-named!(get_tags<&str, Vec<HTML_Tag>>,
-        many1!(complete!(get_tag))
-    );
+//
+//named!(html_tag_type<&str,&str>,
+//    ws!(complete!(nom::alphanumeric)) //might be alphanumeric?
+//);
+//
+//named!(html_start_tag<&str,(&str,&str,&str)>,
+//        tuple!(
+//            tag_s!("<"),
+//            html_tag_type,
+//            tag_s!(">")
+//        )
+//);
+//
+//named!(html_end_tag<&str,(&str,&str,&str)>,
+//        tuple!(
+//            tag_s!("</"),
+//            html_tag_type,
+//            tag_s!(">")
+//        )
+//);
+//
+//
+//#[derive(Debug)]
+//struct HTML_Tag {
+//    tag_type: String,
+//    content: String,
+//}
+//
+//named!(get_tag<&str, HTML_Tag>,
+//        do_parse!(
+//            start: html_start_tag >>
+//            c: complete!(nom::alphanumeric) >>
+//            html_end_tag
+//            >>
+//            (HTML_Tag{tag_type: {}start.1.to_owned(), content: c.to_owned()}) //check if start tag == end tag
+//        )
+//    );
+//
+//named!(get_tags<&str, Vec<HTML_Tag>>,
+//        many1!(complete!(get_tag))
+//    );
 
 
 fn main() {
-    let res = html_start_tag("<foo >");
-    println!("{:?}",res);
-    let res = html_end_tag("</foo >");
-    println!("{:?}",res);
-    let res = get_tag("<foo >foobar</foo >");
-    println!("{:?}",res);
-    let res = get_tags("<foo>foobar</foo><buz>baz</buz>");
-    println!("{:?}",res);
+    use bsr::rusty_soup;
+
+    let tree = rusty_soup::ParseTree::testing_tree();
+//    let pre_iter = rusty_soup::PtPreOrderIter::new(&tree);
+//    let level_iter = rusty_soup::PtLevelOrderIter::new(&tree);
+//    let links = rusty_soup::PtPreOrderIter::new(&tree).find_tags("a".to_owned());
+
+//    println!("Anchors in html:");
+//    for i in tree.pre_iter().find_tags("a".to_owned()) {
+//        match i {
+//            rusty_soup::HTMLContent::Raw(s) => println!("{}", s),
+//            rusty_soup::HTMLContent::Tag(box_tag) => println!("{}", (**box_tag).get_tag())
+//        }
+//    }
+
+
+    println!("\n PreOrder Traversal:");
+    for i in tree.pre_iter() {
+        match i {
+            rusty_soup::HTMLContent::Raw(s) => println!("{}", s),
+            rusty_soup::HTMLContent::Tag(box_tag) => println!("{}", (**box_tag).get_tag())
+        }
+    }
+
+    println!("\nLevelOrder Traversal:");
+    for i in tree.level_iter() {
+        match i {
+            rusty_soup::HTMLContent::Raw(s) => println!("{}", s),
+            rusty_soup::HTMLContent::Tag(box_tag) => println!("{}", (**box_tag).get_tag())
+        }
+    }
+
+
 }
